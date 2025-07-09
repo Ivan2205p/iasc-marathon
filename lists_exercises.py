72f2125620270b7a6aa98a7189e95aa875db5bba
Ivan2205p <pione2205@gmail.com>
2025-07-09 16:27:12 +0300

    оновив файл

---
+++ lists_exercises.py
@@ -0,0 +1,24 @@
+мій_лист = ["молоко", "хліб", "яйця", "сир", "овочі"]
+мій_лист.append("цукерки")
+print(мій_лист)
+
+grades = (11, 10, 10, 8, 12, 9)
+average = sum(grades) / len(grades)
+print("Average grades:", average)
+
+names = ["Іван", "Петро", "Олег", "Катерина", "Марія"]
+# Sort names in alphabetical order
+names.sort()
+#Print the sortd list
+print(names)
+
+міста = [ "Київ", "Львів", "Одеса", "Харків", "Дніпро"]
+міста[1] = "Вінниця"
+print(міста[0])
+print(міста[-1])
+
+пісні = ["Iron man","Master of puppets", "Paranoid", "Back in the black"]
+print(пісні)
+пісні.remove("Iron man")
+пісні.reverse()
+print(пісні)
